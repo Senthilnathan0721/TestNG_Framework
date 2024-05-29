1ceed33a044154e9f9cac31e6bfd199195775cb4
Senthilnathan V <you@example.comsenthilnathanvairaraj@gmail.com>
2024-05-29 18:24:52 +0530

    pretty added

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
         features = "src/test/resources/Features",
-        glue = "com.stepDefinitions", plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
+        glue = "com.stepDefinitions", plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:", "pretty"},
         tags = "@Regression")
 //plugin = "json:target/cucumber-reports/CucumberTestReport.json")
 
